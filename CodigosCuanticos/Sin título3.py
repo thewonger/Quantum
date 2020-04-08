9c2a7f1c37de154e85119209a7009a24248d90c3
Jorge <jorcallegar@gmail.com>
2020-04-08 14:13:42 +0200

    TensorflowGAN

--- CodigosCuanticos/Sin título3.py
+++ CodigosCuanticos/Sin título3.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr  7 17:46:46 2020
+Created on Tue Apr  7 17:48:06 2020
 
 @author: Jorge
 """
 
+
 from tensorflow import keras
 import numpy as np
+
+
 model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
+
+
+#y=2x -1
+
 xs= np.array([-1.0,0.0,1.0,2.0,3.0,4.0], dtype =float)
 ys = np.array([-3.0,-1.0,1.0,3.0,5.0,7.0],dtype=float)
+
+model.fit(xs,ys,epochs=1000)
+print(model.predict([10]))
+
+
+#le damos x=10 y nos devuelve 19
+
